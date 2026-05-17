a1ef460229bbde7ee9977be8777c0627de0a4663
kareiku <xaxvu7@gmail.com>
2026-05-17 22:48:32 +0200

    Add components back

--- percentage/src/controller.vhdl
+++ percentage/src/controller.vhdl
@@ -3,25 +3,40 @@ use ieee.std_logic_1164.all;
 
 entity controller is
     port (
-        clk     : in  std_logic;
-        length  : in  integer;
-        i       : in  integer;
-        seg     : out std_logic_vector(6 downto 0);
-        an      : out std_logic_vector(3 downto 0)
+        clk    : in  std_logic;
+        length : in  integer;
+        i      : in  integer;
+        seg    : out std_logic_vector(6 downto 0);
+        an     : out std_logic_vector(3 downto 0)
     );
 end entity controller;
 
 architecture rtl of controller is
+    component percentage
+        port (
+            length : in  integer;
+            i      : in  integer;
+            result : out integer
+        );
+    end component percentage;
+
+    component dec7seg
+        port (
+            num : in  integer;
+            seg : out std_logic_vector(6 downto 0)
+        );
+    end component dec7seg;
+
     signal percent : integer := 0;
 begin
-    module1 : entity work.percentage
+    module1 : percentage
         port map (
             length => length,
             i      => i,
             result => percent
         );
 
-    module2 : entity work.dec7seg
+    module2 : dec7seg
         port map (
             num => percent,
             seg => seg
