9c0411e79e27546ea57fd059963e6f4df96437ad
kareiku <xaxvu7@gmail.com>
2026-05-17 04:36:38 +0200

    Update percentage

--- percentage/src/percentage.vhdl
+++ percentage/src/percentage.vhdl
@@ -2,6 +2,33 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
+package Display is
+    function decode(digit : unsigned(3 downto 0)) return std_logic_vector;
+end package Display;
+
+package body Display is
+    function decode(digit : unsigned(3 downto 0)) return std_logic_vector is
+    begin
+        case to_integer(digit) is
+            when 0      => return "0000001";
+            when 1      => return "1001111";
+            when 2      => return "0010010";
+            when 3      => return "0000110";
+            when 4      => return "1001100";
+            when 5      => return "0100100";
+            when 6      => return "0100000";
+            when 7      => return "0001111";
+            when 8      => return "0000000";
+            when 9      => return "0000100";
+            when others => return "1111111";
+        end case;
+    end function;
+end package body Display;
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+
 entity Percentage is
     port (
         clk    : in  std_logic;
@@ -20,6 +47,8 @@ architecture RTL of Percentage is
 
     signal digit0  : unsigned(3 downto 0) := (others => '0');
     signal digit1  : unsigned(3 downto 0) := (others => '0');
+    signal tmp0    : unsigned(31 downto 0) := (others => '0');
+    signal tmp1    : unsigned(31 downto 0) := (others => '0');
 
     signal refresh : unsigned(3 downto 0) := (others => '0');
     signal sel     : std_logic_vector(1 downto 0) := "01";
@@ -34,7 +63,11 @@ begin
                 i <= (others => '0');
                 percent <= (others => '0');
             else
-                if length > 0 then
+                if pulse = '1' then
+                    i <= i + 1;
+                end if;
+
+                if length /= 0 then
                     percent <= (i * 100) / length;
                 else
                     percent <= (others => '0');
@@ -47,8 +80,10 @@ begin
         end if;
     end process;
 
-    digit0 <= percent(3 downto 0) mod 10;
-    digit1 <= (percent / 10)(3 downto 0);
+    tmp0   <= to_unsigned(to_integer(percent mod 10), 32);
+    digit0 <= tmp0(3 downto 0);
+    tmp1   <= to_unsigned(to_integer((percent / 10) mod 10, 32));
+    digit1 <= tmp1(3 downto 0);
 
     process(clk)
     begin
@@ -57,23 +92,7 @@ begin
         end if;
     end process;
 
-    sel <= refresh(16 downto 15);
-
-    function decode_seg(digit : unsigned(3 downto 0)) return std_logic_vector is
-    begin
-        case to_integer(digit) is
-            when 0 => return "0000001";
-            when 1 => return "1001111";
-            when 2 => return "0010010";
-            when 3 => return "0000110";
-            when 4 => return "1001100";
-            when 5 => return "0100100";
-            when 6 => return "0100000";
-            when 7 => return "0001111";
-            when 8 => return "0000000";
-            when 9 => return "0000100";
-        end case;
-    end function;
+    sel <= refresh(3 downto 2);
 
     process(sel, digit0, digit1)
     begin
@@ -83,10 +102,10 @@ begin
                 seg <= "1111111";
             when "01" =>
                 an  <= "1101";
-                seg <= decode_seg(digit0);
+                seg <= decode(digit0);
             when "10" =>
                 an  <= "1011";
-                seg <= decode_seg(digit1);
+                seg <= decode(digit1);
             when "11" =>
                 an  <= "1111";
                 seg <= "1111111";
