6ad1d8cf1b14f5be9013d0f420ce07b993bc964b
kareiku <xaxvu7@gmail.com>
2026-05-18 00:27:43 +0200

    Simplify the controller
    It only joins other modules now

--- percentage/src/controller.vhdl
+++ percentage/src/controller.vhdl
@@ -3,7 +3,6 @@ use ieee.std_logic_1164.all;
 
 entity controller is
     port (
-        clk    : in  std_logic;
         length : in  integer;
         i      : in  integer;
         seg    : out std_logic_vector(6 downto 0);
@@ -23,33 +22,24 @@ architecture rtl of controller is
     component dec7seg
         port (
             num : in  integer;
-            seg : out std_logic_vector(6 downto 0)
+            seg : out std_logic_vector(6 downto 0);
+            an  : out std_logic_vector(3 downto 0)
         );
     end component dec7seg;
 
     signal percent : integer := 0;
 begin
-    module1 : percentage
+    module1: percentage
         port map (
             length => length,
             i      => i,
             result => percent
         );
 
-    module2 : dec7seg
+    module2: dec7seg
         port map (
             num => percent,
-            seg => seg
+            seg => seg,
+            an  => an
         );
-
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            if percent > 10 then
-                an <= "1011";
-            else
-                an <= "1101";
-            end if;
-        end if;
-    end process;
 end architecture rtl;
