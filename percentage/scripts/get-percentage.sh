7728669b1b38de747516846bb6c75a72f2f8fe97
kareiku <xaxvu7@gmail.com>
2026-05-17 02:24:09 +0200

    Fix: remove spaces between '='

--- percentage/scripts/get-percentage.sh
+++ percentage/scripts/get-percentage.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 [ $# -lt 2 ] && echo "Usage: $0 <filepath>" && exit 1
 
 if [ -f "$1" ]; then
-    line_count_file = "$(mktemp)"
+    line_count_file="$(mktemp)"
     wc -l "$1" | tee "${line_count_file}"
     echo "Line count has been stored in ${line_count_file}"
     exit 0
