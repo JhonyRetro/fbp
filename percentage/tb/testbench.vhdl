04988e4d8c2492904b74455416442ac1d51f96ed
kareiku <xaxvu7@gmail.com>
2026-05-17 22:25:01 +0200

    Update tb

--- percentage/tb/testbench.vhdl
+++ percentage/tb/testbench.vhdl
@@ -1,21 +1,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-library work;
-use work.all;
-
 entity testbench is
 end entity testbench;
 
 architecture test of testbench is
     signal   clk    : std_logic := '0';
-    signal   length : integer := 0;
+    signal   length : integer := 100;
     signal   i      : integer := 0;
     signal   seg    : std_logic_vector(6 downto 0);
     signal   an     : std_logic_vector(3 downto 0);
+
     constant period : time := 10 ns;
 begin
-    ctl: entity work.controller
+    uut: entity work.controller
         port map (
             clk    => clk,
             length => length,
@@ -34,10 +32,16 @@ begin
         end loop;
     end process;
 
-    process(clk)
+    stimulus: process(clk)
     begin
         if rising_edge(clk) then
             i <= i + 1;
         end if;
+    end process;
+
+    stop : process
+    begin
+        wait for 2 us;
+        assert false report "END" severity failure;
     end process;
 end architecture test;
